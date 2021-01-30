c44e1f7c3723111f60b006b3a912c7eb436f1f8d
Anders Sommer Lassen <anders.lassen@icloud.com>
2021-01-30 18:47:25 +0100

    Initial Commit

---
+++ EasyShopper/EasyShopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyShopperApp.swift
+//  EasyShopper
+//
+//  Created by Anders Lassen on 30/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct EasyShopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
